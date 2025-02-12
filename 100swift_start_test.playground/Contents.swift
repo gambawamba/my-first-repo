57506f0dbc877f299b61139fca79999f3929d2ce
Sofia Romanova <zzzzya@gmail.com>
2025-02-12 23:51:25 +0100

    day 1

--- 100swift_start_test.playground/Contents.swift
+++ 100swift_start_test.playground/Contents.swift
@@ -1,3 +1,68 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+
+
+
+
+
+
+
+//
+//var greeting = "Hello, playground"
+//var name = "Sofia"
+//name = "Maria"
+//
+//let character = "Vasia"
+//
+//print(name)
+//name = "Angela"
+//print(name)
+//name = "Marusia"
+//print(name)
+//
+//let actor = "Denzel Washington"
+//let movie = """
+//A day in
+//the life of an
+//Apple engineer
+//"""
+//let nameLengh = actor.count
+//print(nameLengh)
+//print(movie.uppercased())
+//
+//print(movie.hasPrefix("A day"))
+//
+//let score = 10
+//let big = 10_000_000
+//
+//let lowerScore = score - 2
+//let higherScore = score + 10
+//let doubledScore = score * 2
+//let squaredScore = score * score
+//let halvedScore = score / 2
+//print(score)
+//
+//var counter = 10
+//counter += 5
+//print(counter)
+//
+//counter *= 2
+//print(counter)
+//counter -= 10
+//print(counter)
+//counter /= 2
+//print(counter)
+//
+//let number = 120
+//print(number.isMultiple(of: 7))
+//print(120.isMultiple(of: 3))
+//
+//
+//
